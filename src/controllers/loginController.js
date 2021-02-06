4867aea63a25fb3c0f11405832d3163248f50a49
Jamallc <weberrocha15@gmail.com>
2021-02-06 14:52:08 -0300

    :construction: feat: create user register

--- src/controllers/loginController.js
+++ src/controllers/loginController.js
@@ -4,7 +4,25 @@ exports.index = (req, res) => {
   res.render('login')
 }
 
-exports.register = (req, res) => {
-  const login = new Login(req.body)
-  res.send(login.body)
+exports.register = async (req, res) => {
+  try {
+    const login = new Login(req.body);
+    await login.register();
+  
+    if(login.erros.length > 0) {
+      req.flash('erros', login.erros);
+      req.session.save(function(){
+        return res.redirect('/login');
+      });
+      return;
+    }
+    req.flash('success', 'Seu usuário foi criado com sucesso!');
+    req.session.save(function(){
+      return res.redirect('/login');
+    });
+  }catch(e){
+    console.log(e)
+    return res.render('404')
+  }
+
 } 
--- src/middlewares/middleware.js
+++ src/middlewares/middleware.js
@@ -1,15 +1,6 @@
-// module.exports = (req, res, next) => {
-//   if(req.body.cliente) {
-//     req.body.cliente = req.body.cliente.replace('Rocha', 'Não use ROCHA') 
-//     console.log()
-//     console.log(`Vi que você postou ${req.body.cliente}`)
-//     console.log()
-//   }
-//   next();
-// };
-
 exports.middlewareGlobal = (req, res, next) => {
-  res.locals.ummaVarialvelLocal = 'Este é o valor da variável Loocal!'
+  res.locals.erros = req.flash('erros');
+  res.locals.success = req.flash('success');
   next();
 };
 
